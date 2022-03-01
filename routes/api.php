50034a4f40da1434e6003daacab77ec9ffb207b1
analourdescr <ana_lourdes98@hotmail.com>
2022-02-28 18:58:00 -0700

    encuesta

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::get('/prueba', function () {
+    $respuesta = array();
+    $respuesta["nombre"] = "Paula";
+    $respuesta["apellidos"] = "Remy";
+    return $respuesta;
+});*/
+
+Route::post('/respuestas', 
+    'Api\RespuestasController@store')->
+    name('api.respuestas.store');
+    
+Route::get('/tiposusuario',
+    'Api\TiposUsuarioController@index')->
+    name('api.tiposusuario.index');
